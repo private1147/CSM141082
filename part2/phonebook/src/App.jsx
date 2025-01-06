71fd6245c386521f678184da1f5a79ff0cf8690b
private1147 <tsayem1147@gmail.com>
2025-01-06 20:14:44 +0000

    Complete ex 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Persons = ({ persons, showAll, filteredPersons }) => {
   if (showAll){
@@ -48,20 +49,27 @@ function isValidNumber(number) {
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [filteredPersons, setFilteredPersons] = useState([])
   const [filteredName, setFilteredName] = useState('')
 
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('https://crispy-space-garbanzo-x5vr4vqj7vg4cp7pv-3001.app.github.dev/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
+
   const handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault() // VERY IMPORTANT FOR PREVENTING RELOADING
     const personObject = {
       name: newName,
       number: newNumber,
@@ -119,7 +127,7 @@ const App = () => {
 
       <h2>Numbers</h2>
       <Persons persons={persons} showAll={showAll} filteredPersons={filteredPersons} />
-      
+
     </div>
   )
 }
