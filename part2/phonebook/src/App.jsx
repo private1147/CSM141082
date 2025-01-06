95f5b77ebdaa6b2cc1370845c6720fb788be3042
private1147 <tsayem1147@gmail.com>
2025-01-06 18:45:03 +0000

    Complete ex 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,17 @@ import { useState } from 'react'
 
 const Persons = ({ persons }) => {
   return (
-    persons.map((personObject) => <p key={personObject.id}>{personObject.name}</p>)
+    persons.map((personObject) => <p key={personObject.id}>{personObject.name} {personObject.number}</p>)
   )
 }
 
+function isValidNumber(number) {
+  if (number.length === 0) return true
+  const newCharCode = number.toLowerCase().charCodeAt(number.length - 1)
+  if (newCharCode == 32 || newCharCode == 40 || newCharCode == 41 || newCharCode == 45) return true
+  else if (newCharCode >= 49 && newCharCode <= 57) return true
+  else return false
+}
 
 
 const App = () => {
@@ -13,31 +20,45 @@ const App = () => {
     { name: 'Arto Hellas', id: 1 }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1
     }
-    // if (persons.find((person) => person.name.toLowerCase() === newName.toLowerCase() ) !== undefined) {
-    //   alert(newName + " is already added to phonebook")
-    // } else {
-    //   setPersons(persons.concat(personObject))
-    //   setNewName('')
-    // }
-    setPersons(persons.concat(personObject))
-    setNewName('')
+    if (persons.find((person) => person.name.toLowerCase() === newName.toLowerCase() ) !== undefined) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
   }
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
+
+  const handleNumberChange = (event) => {
+    const currentNumber = event.target.value
+    // console.log(`Current number: ${currentNumber}`)
+    if (isValidNumber(currentNumber)) {
+      setNewNumber(currentNumber)
+    } else {
+      alert(`${currentNumber} isn't a valid number due to invalid new character ${currentNumber[currentNumber.length - 1]}`)
+    }
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
